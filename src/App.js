ff0a76b6069a0acaaffef5dac6580b3ddb404e62
nphuong184 <nphuong.1841@gmail.com>
2022-09-21 20:47:47 +0700

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Học tốt react nha bé !</h1>
+      <button name="" id="" class="btn btn-primary" href="#" role="button">
+        Buttun
+      </button>
+    </div>
+  );
+}
+
+export default App;
